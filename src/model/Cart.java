50d7f563f5d2deb4e11794439828febc17d51eb1
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2019-05-31 15:41:32 -0300

    create Cart, Client and Product models

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Product> productList;
+
+    public Cart() {
+        productList = new ArrayList<>();
+    }
+
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(Product product) {
+        productList.add(product);
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "productList=" + productList +
+                '}';
+    }
 }
