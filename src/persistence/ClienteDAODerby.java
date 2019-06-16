e0a560a15598bf053d3b5705fe9949e6610c642b
Eduardo Schweitzer <eduardo.schweitzer@acad.pucrs.br>
2019-06-16 19:09:30 -0300

    Adicionado classes DAO para o pacote de persistência

---
+++ src/persistence/ClienteDAODerby.java
@@ -0,0 +1,33 @@
+package persistence;
+
+import java.util.List;
+
+import business.Client;
+
+public class ClienteDAODerby implements ClientDAO{
+
+	@Override
+	public void put(Client c) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void patch(Client c) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Client get(String cpf) throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Client> getAll() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/persistence/ProductDAODerby.java
@@ -0,0 +1,33 @@
+package persistence;
+
+import java.util.List;
+
+import business.Product;
+
+public class ProductDAODerby implements ProductDAO{
+
+	@Override
+	public void put(Product p) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void patch(Product p) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product get(String desc) throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getAll() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
