f6354a4360ac6abbd3a557f836f2c6422be02810
PatrickMazzuco <patrick.flores@acad.pucrs.br>
2019-06-16 17:58:42 -0300

    Create Payment classes

---
+++ src/business/PaymentBoleto.java
@@ -0,0 +1,8 @@
+package business;
+
+public class PaymentBoleto implements Payment{
+    @Override
+    public void pay(String clientId) {
+        // Payment Method
+    }
+}
---
+++ src/business/PaymentDebitCard.java
@@ -0,0 +1,8 @@
+package business;
+
+public class PaymentDebitCard implements Payment{
+    @Override
+    public void pay(String clientId) {
+        // Payment Method
+    }
+}
