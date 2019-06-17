e76f747984f2fae34bd59331c1d79227f5c522fe
EDUARDO SCHWEITZER NUNES DA SILVA <eduardo.schweitzer@acad.pucrs.br>
2019-06-17 19:50:42 -0300

    Update Facade

--- src/business/PaymentFacade.java
+++ src/business/PaymentFacade.java
@@ -1,11 +1,7 @@
 package business;
 
-import ui.Window;
-import business.Cart;
-import business.Payment;
-
 public class PaymentFacade {
 	
-	public void pay(Window w, Cart c, Payment p) {}
+	public void pay(String clientId, String paymentType) {}
 
 }
